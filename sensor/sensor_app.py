12c756286bdac5f2987774266391a2bd5f66ad1f
Carlos <cagalmarini@hotmail.com>
2022-01-12 10:52:17 -0300

    Update sensor_app.py

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -51,11 +51,11 @@ particle_data = ParticleData(data)
 recs = particle_data.get_data_by_area(rec_area = test_area)
 print("\nHouse Particle sensor records for area {} = {}".format(test_area, len(recs)))
 
-concentrations = particle_data.get_data_concentrations(recs)
+concentrations = particle_data.get_data_concentrations(data = recs)
 print("\tGood Air Quality Recs: {}".format(concentrations["good"]))
 print("\tModerate Air Quality Recs: {}".format(concentrations["moderate"]))
 print("\tBad Air Quality Recs: {}".format(concentrations["bad"]))
 
-recs = particle_data.get_data_by_date(recs)
+recs = particle_data.get_data_by_date(rec_date = test_date)
 print("\nHouse Particle sensor records for date: {} = {}".format( test_date.strftime("%m/%d/%y"), len(recs)))
 # Module 5 code here:
